948a48418a3137d63ab1fe691a41566a61f53f48
Maxinjector <official.ayoadekola@gmail.com>
2024-09-18 19:42:35 +0100

    fix/auto-slider

--- components/TrustedBy.vue
+++ components/TrustedBy.vue
@@ -3,18 +3,31 @@
     <div class="trusted-by__inner">
       <span>Trusted by:</span>
       <div class="trusted-by__inner__logos">
-        <NuxtImg src="/img/trusted-by1.png" alt="logo-1" />
-        <NuxtImg src="/img/trusted-by2.png" alt="logo-1" />
-        <NuxtImg src="/img/trusted-by3.png" alt="logo-1" />
-        <NuxtImg src="/img/trusted-by4.png" alt="logo-1" />
-        <NuxtImg src="/img/trusted-by5.png" alt="logo-1" />
-        <NuxtImg src="/img/trusted-by6.png" alt="logo-1" />
+        <vue-marquee-slider id="marquee-slider" :speed="20000">
+          <img
+            v-for="(image, index) in logos"
+            :key="index"
+            :src="image"
+            class="trusted-by__inner__logos__logo"
+          />
+        </vue-marquee-slider>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { VueMarqueeSlider } from "vue3-marquee-slider";
+import "@/node_modules/vue3-marquee-slider/dist/style.css";
+
+const logos = [
+  "/img/trusted-by1.png",
+  "/img/trusted-by2.png",
+  "/img/trusted-by3.png",
+  "/img/trusted-by4.png",
+  "/img/trusted-by5.png",
+  "/img/trusted-by6.png",
+];
 </script>
 
 <style>
