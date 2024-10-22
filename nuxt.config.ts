c53d36746a33c8c5cad42e8f136b8eff4d16e248
Maxinjector <official.ayoadekola@gmail.com>
2024-10-22 20:15:08 +0100

    nuxt config

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: false },
+    css: ['~/assets/scss/main.scss'],
+    modules: ['@nuxt/image', '@nuxt/icon', '@nuxtjs/seo'],
+    site: {
+        url: 'https://prokip.africa',
+        name: 'Prokip',
+        description: 'Prokip',
+        defaultLocale: 'en',
+    },
+})
